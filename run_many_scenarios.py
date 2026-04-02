11ea3996af0de3c789a6fd66c81d4728671dbb07
kataph <francesco.picompagno@gmail.com>
2026-04-02 07:07:13 +0200

    removed also resistors in scenarios 9, 10, 11

--- run_many_scenarios.py
+++ run_many_scenarios.py
@@ -290,9 +290,19 @@ if __name__ == "__main__":
     # All scenarios that have simulation support (fault_fns defined).
     simulatable_ids = [s.id for s in SCENARIOS if s.fault_fns is not None]
     
-    for scenario_id in [1,2,3,4,5,6,7,8,9]:
+    # for scenario_id in [1,2,3,4,5,6,7,8,9]:
+    #     # print(f"\n=== Scenario {scenario_id} | SpiceSim + LLM ===")
+    #     num_runs = 10
+    #     assistant = "LLM"
+    #     kwargs = {'num_runs':num_runs, 'base_dir':base_dir, 'forced_scenario':scenario_id, 'assistant':assistant, 'rounds':10, 'skip_runs':False, 'log_dir':log_dir, 'out_file':out_file, 'service':"SpiceSim", 'saboteur':"SpiceSim", 'assistant_model':'gpt-4.1'}#"nf-gpt-4o-2024-08-06"}
+    #     write_separator(
+    #         out_file,
+    #         f"kwargs={kwargs} --- {datetime.now().strftime('%Y-%m-%d %H:%M')}",
+    #     )
+    #     main_args(**kwargs)
+    for scenario_id in [10]:
         # print(f"\n=== Scenario {scenario_id} | SpiceSim + LLM ===")
-        num_runs = 10
+        num_runs = 1
         assistant = "LLM"
         kwargs = {'num_runs':num_runs, 'base_dir':base_dir, 'forced_scenario':scenario_id, 'assistant':assistant, 'rounds':10, 'skip_runs':False, 'log_dir':log_dir, 'out_file':out_file, 'service':"SpiceSim", 'saboteur':"SpiceSim", 'assistant_model':'gpt-4.1'}#"nf-gpt-4o-2024-08-06"}
         write_separator(
@@ -300,6 +310,16 @@ if __name__ == "__main__":
             f"kwargs={kwargs} --- {datetime.now().strftime('%Y-%m-%d %H:%M')}",
         )
         main_args(**kwargs)
+    # for scenario_id in [12]:
+    #     # print(f"\n=== Scenario {scenario_id} | SpiceSim + LLM ===")
+    #     num_runs = 1
+    #     assistant = "LLM"
+    #     kwargs = {'num_runs':num_runs, 'base_dir':base_dir, 'forced_scenario':scenario_id, 'assistant':assistant, 'rounds':10, 'skip_runs':False, 'log_dir':log_dir, 'out_file':out_file, 'service':"SpiceSim", 'saboteur':"SpiceSim", 'assistant_model':'gpt-4.1'}#"nf-gpt-4o-2024-08-06"}
+    #     write_separator(
+    #         out_file,
+    #         f"kwargs={kwargs} --- {datetime.now().strftime('%Y-%m-%d %H:%M')}",
+    #     )
+    #     main_args(**kwargs)
     # kwargs = {'num_runs':1, 'base_dir':base_dir, 'forced_scenario':2, 'assistant':"LLM", 'rounds':10, 'skip_runs':False, 'log_dir':log_dir, 'out_file':out_file, 'service':"SpiceSim", 'saboteur':"SpiceSim", 'assistant_model':'gpt-4.1'}#"nf-gpt-4o-2024-08-06"}
     # write_separator(
     #     out_file,
